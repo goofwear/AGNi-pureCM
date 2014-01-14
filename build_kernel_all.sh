7e12bcfc488c9c720977700638f0d59e7dba389f
psndna88 <psndna88@gmail.com>
2014-01-14 14:56:28 +0530

    Updated v2.6 defconfigs (I9300 & I9305)

--- build_kernel_all.sh
+++ build_kernel_all.sh
@@ -3,15 +3,8 @@ export KERNELDIR=`readlink -f .`
 
 cd $KERNELDIR;
 
-echo "Building I9300 CM-type.....";
+echo "Building I9300 .....";
 ./build_kernel_I9300.sh && sleep 20 && rm .config;
 
-echo "Building I9300 OMNI-type.....";
-./build_kernel_omni_I9300.sh&& sleep 20 && rm .config;
-
-echo "Building I9305 CM-type.....";
-./build_kernel_I9305.sh && sleep 20 && rm .config;
-
-echo "Building I9305 OMNI-type.....";
-./build_kernel_omni_I9305.sh;
-
+echo "Building I9305 .....";
+./build_kernel_I9305.sh;

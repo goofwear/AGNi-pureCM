3797acc2effff0d89330a7a80e851e8410424fd6
psndna88 <psndna88@gmail.com>
2014-06-17 19:03:14 +0530

    Revert "Implement: touchwake KnockON (arter97)"
    This reverts commit c56a8fcc9c0c5b2f99f3a55a68399301b3cabe78.

--- drivers/input/touchscreen/mms_ts.c
+++ drivers/input/touchscreen/mms_ts.c
@@ -151,10 +151,6 @@ struct device *sec_touchscreen;
 static struct device *bus_dev;
 
 int touch_is_pressed = 0;
-static bool knockon_reset = false;
-#ifdef CONFIG_TOUCH_WAKE
-static bool mms_ts_suspended = false;
-#endif
 
 #if defined(CONFIG_TARGET_LOCALE_KOR)
 static int noise_mode_indicator;
@@ -444,7 +440,7 @@ static void set_dvfs_lock(struct mms_ts_info *info, uint32_t on)
 		if (unlikely(tb_freq_level == TOUCHBOOST_FREQ_UNDEFINED)) {
 			ret = exynos_cpufreq_get_level(tb_freq, &info->cpufreq_level);    // Yank : Touchboost switch not yet initalized, lookup frequency level here
 	if (ret < 0) {
-			pr_err("[TSP] exynos_cpufreq_get_level error");
+		pr_err("[TSP] exynos_cpufreq_get_level error");
 			} else {
 				tb_freq_level = info->cpufreq_level;			  // Yank : Update the prefetched level at this stage
 			}
@@ -467,7 +463,7 @@ static void set_dvfs_lock(struct mms_ts_info *info, uint32_t on)
 			if (ret < 0) {
 				pr_err("%s: dev lock failed(%d)\n",\
 							__func__, __LINE__);
-}
+			}
 
 			ret = exynos_cpufreq_lock(DVFS_LOCK_ID_TSP,
 							info->cpufreq_level);
@@ -794,20 +790,7 @@ static irqreturn_t mms_ts_interrupt(int irq, void *dev_id)
 	}
 
 #ifdef CONFIG_TOUCH_WAKE
-	if (mms_ts_suspended) {
-		if (knockon) {
-			if (touch_is_pressed == 0) {
-				if (knockon_reset) {
-					knockon_reset = false;
-					touch_press();
-				} else {
-					knockon_reset = true;
-				}
-			}
-		} else {
-			touch_press();
-		}
-	}
+  touch_press();
 #endif
 
 #if TOUCH_BOOSTER
@@ -3139,8 +3122,6 @@ static void mms_ts_early_suspend(struct early_suspend *h)
 	struct mms_ts_info *info;
 	info = container_of(h, struct mms_ts_info, early_suspend);
 	mms_ts_suspend(&info->client->dev);
-#else
-	mms_ts_suspended = true;
 #endif
 }
 
@@ -3150,8 +3131,6 @@ static void mms_ts_late_resume(struct early_suspend *h)
 	struct mms_ts_info *info;
 	info = container_of(h, struct mms_ts_info, early_suspend);
 	mms_ts_resume(&info->client->dev);
-#else
-	mms_ts_suspended = false;
 #endif
 }
 #endif
@@ -3160,19 +3139,19 @@ static void mms_ts_late_resume(struct early_suspend *h)
 static struct mms_ts_info * touchwake_data;
 void touchscreen_disable(void)
 {
-	if (likely(touchwake_data != NULL))
-		mms_ts_suspend(&touchwake_data->client->dev);
+  if (likely(touchwake_data != NULL))
+    mms_ts_suspend(&touchwake_data->client->dev);
 
-	return;
+    return;
 }
 EXPORT_SYMBOL(touchscreen_disable);
 
 void touchscreen_enable(void)
 {
-	if (likely(touchwake_data != NULL))
-		mms_ts_resume(&touchwake_data->client->dev);
+  if (likely(touchwake_data != NULL))
+    mms_ts_resume(&touchwake_data->client->dev);
 
-	return;
+    return;
 }
 EXPORT_SYMBOL(touchscreen_enable);
 #endif
@@ -3360,10 +3339,10 @@ static int __devinit mms_ts_probe(struct i2c_client *client,
 #endif
 
 #ifdef CONFIG_TOUCH_WAKE
-	touchwake_data = info;
-	if (touchwake_data == NULL)
-		pr_err("[TOUCHWAKE] Failed to set touchwake_data\n");
-#endif
+  touchwake_data = info;
+    if (touchwake_data == NULL)
+    pr_err("[TOUCHWAKE] Failed to set touchwake_data\n");
+#endif  
 
 	sec_touchscreen = device_create(sec_class,
 					NULL, 0, info, "sec_touchscreen");

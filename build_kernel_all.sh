5999f7b4dcbcfa58052a8315675a140e9ec147ca
psndna88 <psndna88@gmail.com>
2014-07-16 10:21:40 +0530

    Add v2.8.4 configs and update build scripts

--- build_kernel_all.sh
+++ build_kernel_all.sh
@@ -6,5 +6,12 @@ cd $KERNELDIR;
 echo "Building I9300 .....";
 ./build_kernel_I9300.sh && sleep 20 && rm .config;
 
+echo "Building I9300 wolf .....";
+./build_kernel_I9300_wolf.sh && sleep 20 && rm .config;
+
 echo "Building I9305 .....";
-./build_kernel_I9305.sh;
+./build_kernel_I9305.sh && sleep 20 && rm .config;
+
+echo "Building I9305 wolf .....";
+./build_kernel_I9305_wolf.sh;
+

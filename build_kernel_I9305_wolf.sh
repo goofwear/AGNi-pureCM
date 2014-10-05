6208db3243bb78f8bede541908ec005bd555936e
psndna88 <psndna88@gmail.com>
2014-10-05 12:46:59 +0530

    Add v2.8.9 configs for I9305

--- build_kernel_I9305_wolf.sh
+++ build_kernel_I9305_wolf.sh
@@ -7,7 +7,7 @@ export ARCH=arm
 
 if [ ! -f $KERNELDIR/.config ];
 then
-  make defconfig psn_i9305_v2.8.4_wolfson_defconfig
+  make defconfig psn_i9305_v2.8.9_wolfson_defconfig
 fi
 
 . $KERNELDIR/.config

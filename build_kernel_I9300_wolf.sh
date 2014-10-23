e70de95f7b767ed879d16ecead420024412e11d6
psndna88 <psndna88@gmail.com>
2014-10-23 21:53:13 +0530

    Add v2.9.0 configs

--- build_kernel_I9300_wolf.sh
+++ build_kernel_I9300_wolf.sh
@@ -7,7 +7,7 @@ export ARCH=arm
 
 if [ ! -f $KERNELDIR/.config ];
 then
-  make defconfig psn_i9300_v2.8.9_wolfson_defconfig
+  make defconfig psn_i9300_v2.9.0_wolfson_defconfig
 fi
 
 . $KERNELDIR/.config
